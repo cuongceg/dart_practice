760809abbdc3c1459be1487237430f8127c28c8a
Đỗ Mạnh Cường <136612157+cuongceg@users.noreply.github.com>
2024-10-15 17:33:57 +0700

    refactor: Create constructor.dart

---
+++ constructor.dart
@@ -0,0 +1,16 @@
+void main() {
+  Person person = Person();
+  print(person.name);
+//  print(person.majority);
+}
+
+class Person{
+  String? name;
+  int? age;
+  String? majority;
+  
+//  Person(this.name,this.age);
+//   Person({this.name,this.age});
+//   Person({this.name = "Cuong",this.age =20});
+//   Person(this.name, this.age, [this.majority = "Computer Engineering"]);
+}
